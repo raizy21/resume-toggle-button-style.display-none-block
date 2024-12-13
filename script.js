6fb7385676f812712c872458a0883f5271d08418
Razvan Andrei <chiper_raizy@yahoo.com>
2024-12-13 06:40:32 +0100

    toggle button js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("toggle-button").onclick = function () {
+  let content = document.getElementById("work-content");
+
+  if (content.style.display === "none") {
+    content.style.display = "block";
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+    content.style.display = "none";
+  }
+};
